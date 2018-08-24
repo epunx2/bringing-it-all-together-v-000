1140b9db568c60d1a254010a8c2b20ef2cc0c482
Eric Chrobak <eric@ericchrobak.com>
2018-08-24 21:52:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -39,8 +39,8 @@ class Dog
     self
   end
 
-  def self.create(name:, breed:)
-    dog = Dog.new(name: name, breed: breed)
+  def self.create(id:, name:, breed:)
+    dog = Dog.new(id: id, name: name, breed: breed)
     dog.save
     dog
   end
